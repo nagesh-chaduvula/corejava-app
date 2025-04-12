ba44cc7fca2fe837a47a27f6a74758c86658e6c9
Nageswarareddy Chaduvula <dt241115@ssnc-corp.global>
2025-04-12 21:37:12 +0530

    generics initial commit

---
+++ src/main/java/com/java/standard/generics/basics/GoldFish.java
@@ -0,0 +1,8 @@
+package com.java.standard.generics.basics;
+
+public class GoldFish {
+    public String swimming() {
+        return "GoldFish swimming...";
+    }
+
+}
---
+++ src/main/java/com/java/standard/generics/basics/StarFish.java
@@ -0,0 +1,7 @@
+package com.java.standard.generics.basics;
+
+public class StarFish {
+    public String swimming() {
+        return "StarFish swimming...";
+    }
+}
